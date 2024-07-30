495a5dff55dd95e09f04556e346c0ac57cce4dd9
Konstantin Tuhay <tugay-k@mail.ru>
2024-07-30 06:21:44 +0300

    add: focus on input (ref)

--- src/pages/MainPage.jsx/index.jsx
+++ src/pages/MainPage.jsx/index.jsx
@@ -1,11 +1,18 @@
+import { useEffect, useRef } from "react";
 import styles from "./index.module.css";
 
 const MainPage = () => {
+  const focusOnInput = useRef(null);
+
+  useEffect(() => {
+    focusOnInput.current.focus();
+  });
+
   return (
     <>
       <div className={styles.content}>
         <h1>Search video</h1>
-        <input type="text" />
+        <input type="text" ref={focusOnInput} />
         <button>Search</button>
       </div>
     </>
