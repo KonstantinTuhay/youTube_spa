3cb394e1f65e3a1ba544d04a12a0ecfaee45f1b6
Konstantin Tuhay <tugay-k@mail.ru>
2024-08-03 22:59:06 +0300

    fix: store (provider)

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-// import { store } from "./redux/store.js";
+import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <Provider store={store}>
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
         <App />
