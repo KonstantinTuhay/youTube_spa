344731fa547865373f69e6d5922e7715540cb7eb
Konstantin Tuhay <tugay-k@mail.ru>
2024-08-03 22:58:23 +0300

    delete: RTK Query

--- src/components/ListMovies/index.jsx
+++ src/components/ListMovies/index.jsx
@@ -1,5 +1,29 @@
+import { useGetMoviesQuery } from "../../redux/apiGetMovies";
+
 const ListMovies = () => {
-  return <div>index</div>;
+  const { data: tasks, error, isLoading } = useGetMoviesQuery();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  // console.log(Array.isArray(tasks));
+  console.log(tasks);
+
+  return (
+    <>
+      <div>
+        {(tasks.length === 0 || undefined) && ""}
+        {tasks.map((item) => {
+          <div>{item.etag}</div>;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default ListMovies;
