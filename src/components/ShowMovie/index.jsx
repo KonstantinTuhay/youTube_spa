1ac7d1444adcf72308d63861aec57a24c6bcd8de
Konstantin Tuhay <tugay-k@mail.ru>
2024-08-24 23:15:19 +0300

    delete: comments

--- src/components/ShowMovie/index.jsx
+++ src/components/ShowMovie/index.jsx
@@ -75,29 +75,9 @@ const ShowMovie = ({ movie }) => {
             </Link>
           </List>
         )}
-        {/* <Card
-          hoverable
-          style={{
-            width: 245,
-            height: 138,
-          }}
-          cover={
-            <img alt="Image Movie" src={movie.snippet.thumbnails.default.url} />
-          }
-        >
-          <Meta
-            className={styles.videoTitle}
-            title={movie.snippet.title}
-            description={movie.snippet.channelTitle}
-          />
-        </Card> */}
-
-        {/* <p className={styles.videoTitle}>{movie.snippet.title}</p> */}
-        {/* <p className={styles.author}>{movie.snippet.channelTitle}</p> */}
       </div>
     </>
   );
-  // return <>аывтаавыолаыркгкруцщшк</>;
 };
 
 export default ShowMovie;
