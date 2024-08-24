cde5fbcbfc14ee1cd98d330a6f2ca6349feb47fa
Konstantin Tuhay <tugay-k@mail.ru>
2024-08-24 23:56:28 +0300

    delete: empty styles

--- src/components/ListMovies/index.jsx
+++ src/components/ListMovies/index.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import { useGetMoviesQuery } from "../../redux/apiMovies";
-import { useDispatch, useSelector } from "react-redux";
 import ShowMovie from "../ShowMovie";
-import CircularProgress from "@mui/material/CircularProgress";
 import SearchSystem from "../SearchSystem";
 import ModalWindow from "../ModalWindow";
+import { useGetMoviesQuery } from "../../redux/apiMovies";
+import { switchCard } from "../../redux/slices/switchCards";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import CircularProgress from "@mui/material/CircularProgress";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import ListIcon from "@mui/icons-material/List";
 import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
-import { switchCard } from "../../redux/slices/switchCards";
 import styles from "./index.module.css";
 
 const ListMovies = () => {
@@ -18,7 +18,6 @@ const ListMovies = () => {
   const dispatch = useDispatch();
   const text = useSelector((state) => state.getSlice);
   const isSwitch = useSelector((state) => state.switchCards);
-  console.log(isSwitch);
 
   const { data: movies, error, isLoading } = useGetMoviesQuery(text);
 
@@ -39,22 +38,13 @@ const ListMovies = () => {
     <>
       <ModalWindow open={open} setOpen={setOpen} />
 
-      <div className={styles.content}>
-        <SearchSystem
-          open={open}
-          setOpen={setOpen}
-          // className={styles.searchSystem}
-        />
+      <div>
+        <SearchSystem open={open} setOpen={setOpen} />
 
         <div className={styles.buttDirection}>
           <h4>Video on demand {`"${text}"`}</h4>
 
-          <ToggleButtonGroup
-            // value={alignment}
-            exclusive
-            // onChange={handleAlignment}
-            aria-label="text alignment"
-          >
+          <ToggleButtonGroup exclusive aria-label="text alignment">
             <ToggleButton
               value="left"
               aria-label="left aligned"
