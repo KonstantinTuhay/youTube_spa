a91b300af4099b6245201b03530ceba8d4b17471
Konstantin Tuhay <tugay-k@mail.ru>
2024-08-26 23:21:17 +0300

    update: divide menu of components

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,174 +1,23 @@
-import { useState } from "react";
-import { NavLink } from "react-router-dom";
-import { styled } from "@mui/material/styles";
-import {
-  IconButton,
-  FormControlLabel,
-  Divider,
-  ListItemIcon,
-  MenuItem,
-  Menu,
-  Avatar,
-  Switch,
-} from "@mui/material";
-import PersonAdd from "@mui/icons-material/PersonAdd";
-import { Settings, Logout } from "@mui/icons-material";
+import Logo from "../Logo";
+import NavPages from "../NavPages";
+import AccountSettings from "../AccountSettings";
+import SwitchDayNight from "../SwitchDayNight";
 import Typography from "@mui/material/Typography";
-import { SiYoutubeshorts } from "react-icons/si";
-import styles from "./index.module.css";
+import Tooltip from "@mui/material/Tooltip";
 
 const Navigation = () => {
-  const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-    width: 62,
-    height: 34,
-    padding: 7,
-    "& .MuiSwitch-switchBase": {
-      margin: 1,
-      padding: 0,
-      transform: "translateX(6px)",
-      "&.Mui-checked": {
-        color: "#fff",
-        transform: "translateX(22px)",
-        "& .MuiSwitch-thumb:before": {
-          backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-            "#fff"
-          )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
-        },
-        "& + .MuiSwitch-track": {
-          opacity: 1,
-          backgroundColor:
-            theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
-        },
-      },
-    },
-    "& .MuiSwitch-thumb": {
-      backgroundColor: theme.palette.mode === "dark" ? "#ffffff" : "#000000",
-      width: 32,
-      height: 32,
-      "&::before": {
-        content: "''",
-        position: "absolute",
-        width: "100%",
-        height: "100%",
-        left: 0,
-        top: 0,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-          "#fff"
-        )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
-      },
-    },
-    "& .MuiSwitch-track": {
-      opacity: 1,
-      backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
-      borderRadius: 20 / 2,
-    },
-  }));
-
-  const [anchorEl, setAnchorEl] = useState(null);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const open = Boolean(anchorEl);
-
   return (
     <>
       <Typography sx={{ align: "left" }}>
-        <NavLink to="/main">
-          <SiYoutubeshorts style={{ fontSize: "50px", color: "black" }} />
-        </NavLink>
+        <Logo />
       </Typography>
       <Typography sx={{ display: "flex", flexGrow: 1, pl: 24 }}>
-        <Typography>
-          <NavLink className={styles.link} to="/main">
-            Search
-          </NavLink>
-        </Typography>
-        <Typography>
-          <NavLink className={styles.link} to="/favorites">
-            Favorites
-          </NavLink>
-        </Typography>
+        <NavPages />
       </Typography>
-      <FormControlLabel control={<MaterialUISwitch defaultChecked />} />
-      <IconButton
-        onClick={handleClick}
-        size="small"
-        aria-controls={open ? "account-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-      >
-        <Avatar sx={{ width: 32, height: 32, backgroundColor: "#000000" }}>
-          M
-        </Avatar>
-      </IconButton>
-      <Menu
-        anchorEl={anchorEl}
-        id="account-menu"
-        open={open}
-        onClose={handleClose}
-        onClick={handleClose}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: "visible",
-            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-            mt: 1.5,
-            "& .MuiAvatar-root": {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
-            },
-            "&::before": {
-              content: '""',
-              display: "block",
-              position: "absolute",
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: "background.paper",
-              transform: "translateY(-50%) rotate(45deg)",
-              zIndex: 0,
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: "right", vertical: "top" }}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-      >
-        <MenuItem onClick={handleClose}>
-          <Avatar /> Profile
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <Avatar /> My account
-        </MenuItem>
-        <Divider />
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <PersonAdd fontSize="small" />
-          </ListItemIcon>
-          Add another account
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Settings fontSize="small" />
-          </ListItemIcon>
-          Settings
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          <NavLink className={styles.logOut} to="/registr">
-            Log out
-          </NavLink>
-        </MenuItem>
-      </Menu>
+      <SwitchDayNight />
+      <Tooltip title="Account settings">
+        <AccountSettings />
+      </Tooltip>
     </>
   );
 };
