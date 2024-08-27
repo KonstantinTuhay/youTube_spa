196ff401d4d7f4bcbb7813f484f713120a9af19c
Konstantin Tuhay <tugay-k@mail.ru>
2024-08-27 17:40:37 +0300

    update: delete import style

--- src/components/ListMovies/index.jsx
+++ src/components/ListMovies/index.jsx
@@ -5,14 +5,13 @@ import { useGetMoviesQuery } from "../../redux/apiMovies";
 import { switchCard } from "../../redux/slices/switchCards";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import ListIcon from "@mui/icons-material/List";
 import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
-import Box from "@mui/material/Box";
-import { Typography } from "@mui/material";
-// import styles from "./index.module.css";
 
 const ListMovies = () => {
   const [open, setOpen] = useState(false);
