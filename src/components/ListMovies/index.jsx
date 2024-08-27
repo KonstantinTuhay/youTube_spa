1c1c0db9701f57300c9da32fb4c267f766857429
Konstantin Tuhay <tugay-k@mail.ru>
2024-08-27 16:49:30 +0300

    update: styles in Object MUI

--- src/components/ListMovies/index.jsx
+++ src/components/ListMovies/index.jsx
@@ -11,6 +11,7 @@ import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import ListIcon from "@mui/icons-material/List";
 import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
 import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
 import styles from "./index.module.css";
 
 const ListMovies = () => {
@@ -24,8 +25,15 @@ const ListMovies = () => {
 
   if (isLoading) {
     return (
-      <Box>
-        <CircularProgress color="inherit" />;
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          mt: "6rem",
+          textAlign: "center",
+        }}
+      >
+        <CircularProgress color="inherit" />
       </Box>
     );
   }
@@ -34,14 +42,32 @@ const ListMovies = () => {
     return <p>Error: {error.message}</p>;
   }
 
+  const style = {
+    width: "100%",
+    textAlign: "start",
+    variant: "h3",
+    marginBottom: "10px",
+  };
+
   return (
     <>
       <ModalWindow open={open} setOpen={setOpen} />
 
-      <div>
-        <SearchSystem open={open} setOpen={setOpen} />
-        <div className={styles.buttDirection}>
-          <h4>Video on demand {`"${text}"`}</h4>
+      <Box sx={{ ml: 3, mt: 5 }}>
+        <SearchSystem open={open} setOpen={setOpen} style={style} />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            margin: "25px 0 0",
+            color: "black",
+            lineHeight: 0,
+          }}
+        >
+          <Typography variant="h6" sx={{ fontWeight: 200 }}>
+            Video on demand {`"${text}"`}
+          </Typography>
 
           <ToggleButtonGroup exclusive aria-label="text alignment">
             <ToggleButton
@@ -59,13 +85,13 @@ const ListMovies = () => {
               <GridViewOutlinedIcon />
             </ToggleButton>
           </ToggleButtonGroup>
-        </div>
+        </Box>
         <div className={isSwitch ? styles.allMoviesGrid : styles.allMoviesFlex}>
           {movies.map((movie) => {
             return <ShowMovie key={movie.id.videoId} movie={movie} />;
           })}
         </div>
-      </div>
+      </Box>
     </>
   );
 };
--- src/pages/MainPage.jsx/index.jsx
+++ src/pages/MainPage.jsx/index.jsx
@@ -6,11 +6,18 @@ import styles from "./index.module.css";
 const MainPage = () => {
   const [open, setOpen] = useState(false);
 
+  const style = {
+    width: 900,
+    textAlign: "center",
+    variant: "h2",
+    marginBottom: "50px",
+  };
+
   return (
     <>
       <ModalWindow open={open} setOpen={setOpen} />
       <div className={styles.content}>
-        <SearchSystem open={open} setOpen={setOpen} />
+        <SearchSystem open={open} setOpen={setOpen} style={style} />
       </div>
     </>
   );
