ced2e9d730098177f91a29f900a3e5276b68381c
Konstantin Tuhay <tugay-k@mail.ru>
2024-08-30 22:00:17 +0300

    delete: comments

--- src/components/AccountSettings/index.jsx
+++ src/components/AccountSettings/index.jsx
@@ -1,17 +1,8 @@
-import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import {
-  IconButton,
-  Divider,
-  ListItemIcon,
-  MenuItem,
-  Menu,
-  Avatar,
-} from "@mui/material";
-import { Settings, Logout } from "@mui/icons-material";
-import PersonAdd from "@mui/icons-material/PersonAdd";
-import Stack from "@mui/material/Stack";
 import { useSelector } from "react-redux";
+import { ListItemIcon } from "@mui/material";
+import { Logout } from "@mui/icons-material";
+import Stack from "@mui/material/Stack";
 
 const AccountSettings = () => {
   const dayNightTheme = useSelector((state) => state.switchDayNight);
@@ -42,66 +33,6 @@ const AccountSettings = () => {
           </NavLink>
         </ListItemIcon>
       </Stack>
-      {/* <IconButton
-        onClick={handleClick}
-        size="small"
-        aria-controls={open ? "account-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-      >
-        <Avatar
-          sx={{
-            width: 32,
-            height: 32,
-            backgroundColor: dayNightTheme ? "#000000" : "#ffffff",
-            color: dayNightTheme ? "#ffffff" : "#000000",
-          }}
-        >
-          M
-        </Avatar>
-      </IconButton>
-      <Menu
-        anchorEl={anchorEl}
-        id="account-menu"
-        open={open}
-        onClose={handleClose}
-        onClick={handleClose}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: "visible",
-            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-            mt: 1.5,
-            "& .MuiAvatar-root": {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
-            },
-            "&::before": {
-              content: '""',
-              display: "block",
-              position: "absolute",
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: "background.paper",
-              transform: "translateY(-50%) rotate(45deg)",
-              zIndex: 0,
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: "right", vertical: "top" }}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-      >
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          <NavLink to="/registr">Log out</NavLink>
-        </MenuItem>
-      </Menu> */}
     </>
   );
 };
